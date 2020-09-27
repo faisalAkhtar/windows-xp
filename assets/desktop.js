c4d356a851591ba0978323ac9a38f6816521a355
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2020-09-27 20:22:43 +0530

    ‎

--- assets/desktop.js
+++ assets/desktop.js
@@ -6,3 +6,21 @@ let timeSetter = setInterval(() => {
     time.innerHTML = new Intl.DateTimeFormat('en-IN',{ hour:'numeric',minute:'numeric',hour12:true }).format(new Date())
     time.setAttribute("title", new Intl.DateTimeFormat('en-IN',{ hour:'numeric',minute:'numeric',hour12:true,year:'numeric',month:'long',day:'numeric' }).format(new Date()) )
 }, 60000);
+
+document.querySelector(".desktop").onclick = function() {
+    document.querySelectorAll(".icon").forEach( (e)=>{
+        e.classList.remove("selected")
+    })
+}
+
+document.querySelectorAll(".icon").forEach( (icon)=>{
+    icon.onclick = function() {
+        setTimeout(() => {
+            document.querySelectorAll(".icon").forEach( (e)=>{
+                e.classList.remove("selected")
+            })
+            this.classList.add("selected")
+        }, 1);
+        // alert(this.lastElementChild.innerHTML)
+    }
+})
