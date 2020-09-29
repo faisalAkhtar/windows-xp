850ce7f0c471c93cf7d689e0ef458964a5f4d730
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2020-09-30 00:02:11 +0530

    ‎

--- assets/desktop.js
+++ assets/desktop.js
@@ -1,54 +1,98 @@
 let time = document.querySelector(".time")
-time.innerHTML = new Intl.DateTimeFormat('en-IN',{ hour:'numeric',minute:'numeric',hour12:true }).format(new Date())
-time.setAttribute("title", new Intl.DateTimeFormat('en-IN',{ hour:'numeric',minute:'numeric',hour12:true,year:'numeric',month:'long',day:'numeric' }).format(new Date()) )
+time.innerHTML = new Intl.DateTimeFormat('en-IN', { hour: 'numeric', minute: 'numeric', hour12: true }).format(new Date())
+time.setAttribute("title", new Intl.DateTimeFormat('en-IN', { hour: 'numeric', minute: 'numeric', hour12: true, year: 'numeric', month: 'long', day: 'numeric' }).format(new Date()))
 
 let timeSetter = setInterval(() => {
-    time.innerHTML = new Intl.DateTimeFormat('en-IN',{ hour:'numeric',minute:'numeric',hour12:true }).format(new Date())
-    time.setAttribute("title", new Intl.DateTimeFormat('en-IN',{ hour:'numeric',minute:'numeric',hour12:true,year:'numeric',month:'long',day:'numeric' }).format(new Date()) )
+    time.innerHTML = new Intl.DateTimeFormat('en-IN', { hour: 'numeric', minute: 'numeric', hour12: true }).format(new Date())
+    time.setAttribute("title", new Intl.DateTimeFormat('en-IN', { hour: 'numeric', minute: 'numeric', hour12: true, year: 'numeric', month: 'long', day: 'numeric' }).format(new Date()))
 }, 60000);
 
-document.querySelector(".desktop").onclick = function() {
-    document.querySelectorAll(".icon").forEach( (e)=>{
+document.querySelector(".desktop").onclick = function () {
+    document.querySelectorAll(".icon").forEach((e) => {
         e.classList.remove("selected")
     })
 }
 
-document.querySelectorAll(".icon").forEach( (icon)=>{
-    icon.onclick = function() {
+document.querySelectorAll(".icon").forEach((icon) => {
+    icon.onclick = function () {
         setTimeout(() => {
-            document.querySelectorAll(".icon").forEach( (e)=>{
+            document.querySelectorAll(".icon").forEach((e) => {
                 e.classList.remove("selected")
             })
             this.classList.add("selected")
         }, 1);
-        // alert(this.lastElementChild.innerHTML)
     }
-
-    /*icon.ondblclick = function() {
-        alert(this.lastElementChild.innerHTML)
-    }*/
 })
 
-document.querySelector(".max").onclick = function() {
+document.querySelector(".max").onclick = function () {
     document.querySelector(".window").classList.toggle("maximized")
 }
 
-document.querySelector(".min").onclick = function() {
+document.querySelector(".min").onclick = function () {
     document.querySelector(".readme").classList.toggle("active")
     document.querySelector(".window").classList.toggle("minimized")
 }
 
-document.querySelector(".readme").onclick = function() {
+document.querySelector(".readme").onclick = function () {
     document.querySelector(".readme").classList.toggle("active")
     document.querySelector(".window").classList.toggle("minimized")
 }
 
-document.querySelector(".cls").onclick = function() {
+document.querySelector(".cls").onclick = function () {
     document.querySelector(".readme").style.display = "none"
     document.querySelector(".window").style.display = "none"
 }
 
-document.querySelector(".note-pad").ondblclick = function() {
+document.querySelector(".faisal-akhtar").ondblclick = function () {
+    window.open("https://faisalakhtar.github.io/")
+}
+
+document.querySelector(".my-computer").ondblclick = function () {
+    window.open("https://github.com/faisalAkhtar/")
+}
+
+document.querySelector(".my-network").ondblclick = function () {
+    window.open("https://twitter.com/faisallakhtarr/")
+}
+
+document.querySelector(".note-pad").ondblclick = function () {
     document.querySelector(".readme").style.display = "initial"
     document.querySelector(".window").style.display = "initial"
+}
+
+
+dragWindow(document.querySelector(".window"));
+function dragWindow(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    document.querySelector(".title-bar").onmousedown = dragging
+
+    function dragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onmouseup = stopDragging;
+        document.onmousemove = draggedWindow;
+    }
+
+    function draggedWindow(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function stopDragging() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
